973c5be2312150f71bc9f623eb41d5b309f19901
Lucas Salaverry Félix <lucas.salaverry@edu.pucrs.br>
2020-07-10 23:20:55 -0300

    Mouse Click Adjustments

--- Corporation/Assets/Scripts/CursorController.cs
+++ Corporation/Assets/Scripts/CursorController.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class CursorController : MonoBehaviour
 {
+    private Texture2D mouseTextureIdle;
+  
+    private Texture2D mouseTextureClicking;
 
-    [SerializeField]
-    private Texture2D mouseTexture;
+  
+    void Awake()
+    {
+        mouseTextureIdle = Resources.Load("CursorIdle") as Texture2D;
 
+        mouseTextureClicking = Resources.Load("CursorClick") as Texture2D;
+    }
     void Start()
     {
-        Cursor.SetCursor(mouseTexture,Vector2.zero, CursorMode.Auto);
+        Cursor.SetCursor(mouseTextureIdle, new Vector2(4.5f,0), CursorMode.Auto);
+    }
+
+    void Update()
+    {
+
+
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Cursor.SetCursor(mouseTextureClicking, new Vector2(4.5f, 0), CursorMode.Auto);
+            Debug.Log(mousePos.ToString());
+        }
+        
+        if(Input.GetMouseButtonUp(0))
+        {
+            Cursor.SetCursor(mouseTextureIdle, new Vector2(4.5f, 0), CursorMode.Auto);
+        }    
+
     }
 
     
