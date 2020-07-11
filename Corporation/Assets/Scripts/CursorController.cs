632e22e2056f411f3f96266e32899a59cc423a95
Lucas Salaverry Félix <lucas.salaverry@edu.pucrs.br>
2020-07-10 22:01:14 -0300

    Cursor alterado

---
+++ Corporation/Assets/Scripts/CursorController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Texture2D mouseTexture;
+
+    void Start()
+    {
+        Cursor.SetCursor(mouseTexture,Vector2.zero, CursorMode.Auto);
+    }
+
+    
+}
